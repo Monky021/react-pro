21695320969c9faef8b1f5831ed6a0cc7412ad2d
Andres <carlos.camacho@parquesoftbogota.com>
2022-05-12 17:02:53 -0500

    Rutas anidadas listas

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,4 +1,5 @@
 import { lazy, LazyExoticComponent } from 'react';
+import { NoLazy } from '../01-lazyload/pages/NoLazy';
 
 type JSXComponent = () => JSX.Element;
 
@@ -8,6 +9,7 @@ interface Route {
     name: string,
     Component: LazyExoticComponent<JSXComponent> | JSXComponent
 } 
+const LazyLayout = lazy(() => import('../01-lazyload/layout/LazyLayout'))
 
 const Lazy1 = lazy(() => import('../01-lazyload/pages/LazyPage1'))
 const Lazy2 = lazy(() => import('../01-lazyload/pages/LazyPage2'))
@@ -15,21 +17,16 @@ const Lazy3 = lazy(() => import('../01-lazyload/pages/LazyPage3'))
 
 export const routes: Route[] = [
     {
-        to: '/lazy1',
-        path: 'lazy1',
-        Component: Lazy1,
-        name: 'lazy - 1'
+        path: '/lazyload/*',
+        to: '/lazyload/*',
+        Component: LazyLayout,
+        name: 'lazyLayout'
     },
     {
-        to: '/lazy2',
-        path: 'lazy2',
-        Component: Lazy2,
-        name: 'lazy - 2'
+        to: '/no-lazy',
+        path: 'no-lazy',
+        Component: NoLazy,
+        name: 'No lazy'
     },
-    {
-        to: '/lazy3',
-        path: 'lazy3',
-        Component: Lazy3,
-        name: 'lazy - 3'
-    },
+    
 ]
